2508fc41d684da8fc1831289af11ee261beb858e
rolen <rolen@kakao.com>
2025-02-18 00:55:18 +0900

    [rolen] make result

--- app/src/main/java/com/rolen/myword/QuizActivity.java
+++ app/src/main/java/com/rolen/myword/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.rolen.myword;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.animation.Animation;
@@ -29,6 +30,8 @@ public class QuizActivity extends AppCompatActivity {
     private String[] meanings = {"사과", "바나나", "고양이", "개", "코끼리"};
     private int currentIndex = 0;
     private boolean isEnglishQuestion = true;
+    private int correctCount = 0;
+    private int wrongCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,9 +106,12 @@ public class QuizActivity extends AppCompatActivity {
             tvResult.setText("정답! 🎉");
             tvResult.setTextColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
             score += 10;
+            correctCount++;
         } else {
             tvResult.setText("오답!\n정답: " + correctAnswer);
             tvResult.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_light));
+//            score -= 5;
+            wrongCount++;
         }
 
         updateScore();
@@ -126,7 +132,17 @@ public class QuizActivity extends AppCompatActivity {
                 @Override
                 public void onAnimationEnd(Animation animation) {
                     currentIndex++;
-                    loadNextQuestion();
+                    if (currentIndex >= words.length) {
+                        // 퀴즈 종료 -> 결과 화면으로 이동
+                        Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
+                        intent.putExtra("score", score);
+                        intent.putExtra("correctCount", correctCount);
+                        intent.putExtra("wrongCount", wrongCount);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        loadNextQuestion();
+                    }
                 }
 
                 @Override
