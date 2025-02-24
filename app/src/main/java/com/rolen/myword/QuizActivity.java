bc4847ce132fef79ba3f9c4a13bc7b6063685921
rolen <rolen@kakao.com>
2025-02-24 21:23:45 +0900

    [change] category

--- app/src/main/java/com/rolen/myword/QuizActivity.java
+++ app/src/main/java/com/rolen/myword/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.rolen.myword;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.animation.Animation;
@@ -29,6 +30,8 @@ public class QuizActivity extends AppCompatActivity {
     private String[] meanings = {"사과", "바나나", "고양이", "개", "코끼리"};
     private int currentIndex = 0;
     private boolean isEnglishQuestion = true;
+    private int correctCount = 0;
+    private int wrongCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,9 +106,12 @@ public class QuizActivity extends AppCompatActivity {
             tvResult.setText("정답! 🎉");
             tvResult.setTextColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
             score += 10;
+            correctCount++;
         } else {
             tvResult.setText("오답!\n정답: " + correctAnswer);
             tvResult.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_light));
+            score -= 5;
+            wrongCount++;
         }
 
         updateScore();
@@ -121,21 +127,32 @@ public class QuizActivity extends AppCompatActivity {
             tvQuestion.startAnimation(exitAnimation);
             exitAnimation.setAnimationListener(new Animation.AnimationListener() {
                 @Override
-                public void onAnimationStart(Animation animation) {}
+                public void onAnimationStart(Animation animation) {
+                }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
                     currentIndex++;
-                    loadNextQuestion();
+                    if (currentIndex >= words.length) {
+                        // 퀴즈 종료 -> 결과 화면으로 이동
+                        Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
+                        intent.putExtra("score", score);
+                        intent.putExtra("correctCount", correctCount);
+                        intent.putExtra("wrongCount", wrongCount);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        loadNextQuestion();
+                    }
                 }
 
                 @Override
-                public void onAnimationRepeat(Animation animation) {}
+                public void onAnimationRepeat(Animation animation) {
+                }
             });
         }, 1000);
     }
-
-    private void updateScore() {
-        tvScore.setText("점수: " + score);
-    }
+        private void updateScore () {
+            tvScore.setText("점수: " + score);
+        }
 }
